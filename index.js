3b4cd2e2f357869ee09c23619d8d22ad5ebbc77a
Daniel Lee <danieljlee0814@gmail.com>
2020-04-01 14:46:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,8 +12,9 @@ function logWhisper(string) {
 }
 function sayHiToGrandma(string) {
   if (string === whisper(string)) {
+{
     return "I can't hear you!"}
-  if (string === shout(string)) {
+      if (string === shout(string)) {
     return "YES INDEED!"
   if (string === "I love you, Grandma.") {
     return "I love you, too."
